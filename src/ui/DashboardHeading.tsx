a29b9b68b23f4bcb3fcbdbe102930917d5e3e8d6
withnoclout <wpong.debug@gmail.com>
2025-08-10 11:59:46 +0000

    DashboardHeading: center alignment; adjust second line color to white/60 per reference

--- src/ui/DashboardHeading.tsx
+++ src/ui/DashboardHeading.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export function DashboardHeading() {
   return (
-    <section className="w-[1380px] mx-auto mt-[150px] font-brooklyn">
+    <section className="w-[1380px] mx-auto mt-[150px] font-brooklyn text-center">
       <h2 className="text-[30px] font-semibold">
         <span className="block leading-none text-white">
           Stay productive and manage your app
         </span>
-        <span className="block mt-[25px] leading-none text-emerald-400">
+        <span className="block mt-[25px] leading-none text-white/60">
           without leaving the dashboard
         </span>
       </h2>
