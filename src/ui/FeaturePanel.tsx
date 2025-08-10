6db6bad5a45e72ea04583e68e125b0d124564d1d
withnoclout <wpong.debug@gmail.com>
2025-08-10 08:22:17 +0000

    FeaturePanel: 7-card layout in exact 1350×800, 4 columns with 25px gaps; top 2-1-1, bottom 4; grid-auto-rows 387.5px

--- src/ui/FeaturePanel.tsx
+++ src/ui/FeaturePanel.tsx
@@ -1,32 +1,33 @@
 export default function FeaturePanel() {
   return (
-    <div className="max-w-[1375px] mx-auto h-[825px]">
+    <div className="w-[1350px] h-[800px] mx-auto">
       <div
-        className="grid grid-cols-12 gap-[25px]"
-        style={{ gridAutoRows: '400px' }}
+        className="grid grid-cols-4 gap-[25px]"
+        style={{ gridAutoRows: '387.5px' }}
       >
-        {/* Big Monitoring Dashboard card */}
-  <section className="card col-span-8 row-span-2">
+        {/* Top row: [2 cols | 1 col | 1 col] */}
+        <section className="card col-span-2">
           <h3 className="text-lg font-semibold text-white">Monitoring Dashboard</h3>
         </section>
-
-        {/* Right stack */}
-  <section className="card col-span-4 row-span-1">
+        <section className="card col-span-1">
           <h3 className="text-lg font-semibold text-white">User &amp; Org Access</h3>
         </section>
-  <section className="card col-span-4 row-span-1">
+        <section className="card col-span-1">
           <h3 className="text-lg font-semibold text-white">Edge Collectors</h3>
         </section>
 
-        {/* Bottom row */}
-  <section className="card col-span-4 row-span-1">
+        {/* Bottom row: 4 equal cards */}
+        <section className="card col-span-1">
           <h3 className="text-lg font-semibold text-white">Data Ingestion</h3>
         </section>
-  <section className="card col-span-4 row-span-1">
+        <section className="card col-span-1">
           <h3 className="text-lg font-semibold text-white">Realtime Sensors</h3>
         </section>
-  <section className="card col-span-4 row-span-1">
+        <section className="card col-span-1">
           <h3 className="text-lg font-semibold text-white">Modeling &amp; Forecasts</h3>
+        </section>
+        <section className="card col-span-1">
+          <h3 className="text-lg font-semibold text-white">Reporting &amp; APIs</h3>
         </section>
       </div>
     </div>
